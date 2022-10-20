715d0c30320680f38a26bea0f8ade7d364ff6732
Dharmraj07 <70120849+Dharmraj07@users.noreply.github.com>
2022-10-20 11:55:55 +0530

    Update y1.js

--- y1.js
+++ y1.js
@@ -1,5 +1,7 @@
+
 let item=document.querySelectorAll("li");
 item[1].style.color="green"
 for(i=0; i<item.length; i+=2){
     item[i].style.backgroundColor="green";
 }
+item[2].textContent="     .";
